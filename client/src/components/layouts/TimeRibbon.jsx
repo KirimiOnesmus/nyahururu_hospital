d6aae3678f14608c4520ef0cc23b40b1d75e4f7d
OnesmusKirimi <onesmuskirimi64@gmail.com>
2025-09-28 00:39:15 +0300

    Added the time ribbon

---
+++ client/src/components/layouts/TimeRibbon.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const TimeRibbon = () => {
+  return (
+    <div className="bg-blue-400 text-white py-16">
+      <div className=" flex flex-col justify-center md:flex-row md:justify-around mx-6 space-y-6 md:space-y-0">
+        <div className="hours space-y-2">
+          <div>
+            <h3 className="text-2xl font-semibold mb-2 underline">Work Hours</h3>
+            <p className="font-serif">Monday-Sunday: 24/7</p>
+          </div>
+          <div>
+            {" "}
+            <h3 className="text-2xl font-semibold underline">Visiting Hours</h3>
+            <p className="font-serif">Monday-Sunday: 12noon-2pm, 4pm-6 pm</p>
+          </div>
+        </div>
+        <div className="specialist-hours">
+          <h3 className="text-2xl font-semibold mb-2 underline">Specialist Clinics</h3>
+          <p className="font-serif">Monday-Friday: 8am-5pm</p>
+          <p className="font-serif">Saturday & Sunday: 9am-4pm</p>
+          <button className="mt-2 font-bold rounded-lg border p-2 cursor-pointer">Book Appointment</button>
+        </div>
+        <div>
+          <h3 className="text-2xl font-semibold mb-2 underline">Feedback</h3>
+          <p className="font-serif">We'd love to hear from you.</p>
+          <button className="mt-2 font-bold rounded-lg border p-2 cursor-pointer">Send Feedback</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TimeRibbon;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Header, Footer } from "../components/layouts";
-import Slider from "../components/layouts/Slider";
+import {
+  Header,
+  Footer,
+  Slider,
+  Card,
+  TimeRibbon,
+} from "../components/layouts";
+
 import dialysis from "../assets/Icons/dialysis-icon.png";
 import emergency from "../assets/Icons/emergency-icon.png";
 import critical_care from "../assets/Icons/ICU-icon.png";
 import lab from "../assets/Icons/lab-services.png";
 import radiology from "../assets/Icons/radiology-icon.png";
-import Card from "../components/layouts/Card";
+// import Card from "../components/layouts/Card";
+// import TimeRibbon from "../components/layouts/TimeRibbon";
 
 const Home = () => {
   const services = [
@@ -41,19 +48,22 @@ const Home = () => {
           <Slider />
         </div>
         <div className="services my-4 mx-6 py-4 px-6">
-          <h2 className="text-3xl font-bold my-4 text-center">Our Departments</h2>
+          <h2 className="text-3xl font-bold my-4 text-center">
+            Our Departments
+          </h2>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-2">
-            {
-              services.map((service,index)=>(
-                <Card
-                  key={index}
-                  image={service.image}
-                  title={service.title}
-                  buttonText="Learn More"
-                />
-              ))
-            }
+            {services.map((service, index) => (
+              <Card
+                key={index}
+                image={service.image}
+                title={service.title}
+                buttonText="Learn More"
+              />
+            ))}
           </div>
+        </div>
+        <div className="hours ribbon">
+          <TimeRibbon />
         </div>
       </div>
       <div>
