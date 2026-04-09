53b124ca60ccb2d1d1903571506aa1fc1e866a4b
OnesmusKirimi <onesmuskirimi64@gmail.com>
2026-04-10 02:24:04 +0300

    debugged renaming in the app.js file

--- client/src/pages/research/index.js
+++ client/src/pages/research/index.js
@@ -2,5 +2,6 @@ export { default as ResearchPage } from "./Research";
 export { default as ResearchRegister } from "./Register";
 export { default as PublicResearch } from "./PublicResearch"; 
 export { default as ResearchDashboard } from "./ResearchDashboard";
-export { default as Myprofile } from "./Myprofile";
+export { default as Myprofile } from "./MyProfile";
+
 // export { default as SubmitProposal } from "./SubmitProposal";
